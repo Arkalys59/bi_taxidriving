3038d5abb3a1a881964cdb38a44264e46d62160b
BibiModz <83229685+bibimodz@users.noreply.github.com>
2024-10-09 22:15:19 +0200

    Upload all script

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+-- BibiModz Script https://discord.gg/dr9UuDgT8N
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BibiModz'
+description 'Script Taxi Driving'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+
+dependencies {
+    'ox_lib',
+    'ox_target'
+}
+
+shared_script '@ox_lib/init.lua'
+
+lua54 'yes'
+
+
+-- BibiModz Script https://discord.gg/dr9UuDgT8N
